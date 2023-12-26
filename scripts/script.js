1c4585b666c2319857ec9ec19e33a8ee0a41b580
Elena-Iustina Boca <116492124+Vodboka@users.noreply.github.com>
2023-12-26 17:22:54 +0200

    integrated Leaflet API

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    let canvas=document.getElementById('myCanvas')
+    let context= canvas.getContext('2d');
+    var map = L.map('map').setView([51.505, -0.09], 13);
+}
